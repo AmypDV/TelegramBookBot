472bdff411de0fd8233d663933842191f09989cf
AmypDV <amypdv@yandex.ru>
2024-07-07 09:16:37 +0300

    создал клавиатуру закладок

---
+++ keyboards/bookmarks_kb.py
@@ -0,0 +1,25 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from lexicon.lexicon_ru import LEXICON
+
+
+# Функция для формирования инлайн-клавиатуры на лету
+def create_bookmarks_kb(*args: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON.get(button, button),
+                callback_data=button)
+            )
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=1)
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
